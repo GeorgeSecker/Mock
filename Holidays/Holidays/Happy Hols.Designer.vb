ccef2cfd4f399bd9b642d19320f0217ff8866c0d
GeorgeSecker <116958@newporthigh.co.uk>
2017-03-30 11:07:35 +0100

    Add files via upload

---
+++ Holidays/Holidays/Happy Hols.Designer.vb
@@ -0,0 +1,197 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblFirstName = New System.Windows.Forms.Label()
+        Me.lblLastName = New System.Windows.Forms.Label()
+        Me.lblDOB = New System.Windows.Forms.Label()
+        Me.lblEmail = New System.Windows.Forms.Label()
+        Me.txtFirstName = New System.Windows.Forms.TextBox()
+        Me.txtLastName = New System.Windows.Forms.TextBox()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.dtpDOB = New System.Windows.Forms.DateTimePicker()
+        Me.cmdCheck = New System.Windows.Forms.Button()
+        Me.txtStoreDetails = New System.Windows.Forms.RichTextBox()
+        Me.cmdSave = New System.Windows.Forms.Button()
+        Me.cmdClear = New System.Windows.Forms.Button()
+        Me.cmdLoad = New System.Windows.Forms.Button()
+        Me.cmdDelete = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblFirstName
+        '
+        Me.lblFirstName.AutoSize = True
+        Me.lblFirstName.Location = New System.Drawing.Point(19, 33)
+        Me.lblFirstName.Name = "lblFirstName"
+        Me.lblFirstName.Size = New System.Drawing.Size(60, 13)
+        Me.lblFirstName.TabIndex = 0
+        Me.lblFirstName.Text = "First Name:"
+        '
+        'lblLastName
+        '
+        Me.lblLastName.AutoSize = True
+        Me.lblLastName.Location = New System.Drawing.Point(19, 59)
+        Me.lblLastName.Name = "lblLastName"
+        Me.lblLastName.Size = New System.Drawing.Size(52, 13)
+        Me.lblLastName.TabIndex = 1
+        Me.lblLastName.Text = "Surname:"
+        '
+        'lblDOB
+        '
+        Me.lblDOB.AutoSize = True
+        Me.lblDOB.Location = New System.Drawing.Point(0, 85)
+        Me.lblDOB.Name = "lblDOB"
+        Me.lblDOB.Size = New System.Drawing.Size(71, 13)
+        Me.lblDOB.TabIndex = 2
+        Me.lblDOB.Text = "Date Of Birth:"
+        '
+        'lblEmail
+        '
+        Me.lblEmail.AutoSize = True
+        Me.lblEmail.Location = New System.Drawing.Point(36, 114)
+        Me.lblEmail.Name = "lblEmail"
+        Me.lblEmail.Size = New System.Drawing.Size(35, 13)
+        Me.lblEmail.TabIndex = 3
+        Me.lblEmail.Text = "Email:"
+        '
+        'txtFirstName
+        '
+        Me.txtFirstName.Location = New System.Drawing.Point(85, 33)
+        Me.txtFirstName.Name = "txtFirstName"
+        Me.txtFirstName.Size = New System.Drawing.Size(200, 20)
+        Me.txtFirstName.TabIndex = 4
+        '
+        'txtLastName
+        '
+        Me.txtLastName.Location = New System.Drawing.Point(85, 59)
+        Me.txtLastName.Name = "txtLastName"
+        Me.txtLastName.Size = New System.Drawing.Size(200, 20)
+        Me.txtLastName.TabIndex = 5
+        '
+        'txtEmail
+        '
+        Me.txtEmail.Location = New System.Drawing.Point(85, 111)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(200, 20)
+        Me.txtEmail.TabIndex = 6
+        '
+        'dtpDOB
+        '
+        Me.dtpDOB.Location = New System.Drawing.Point(85, 85)
+        Me.dtpDOB.Name = "dtpDOB"
+        Me.dtpDOB.Size = New System.Drawing.Size(200, 20)
+        Me.dtpDOB.TabIndex = 7
+        '
+        'cmdCheck
+        '
+        Me.cmdCheck.Location = New System.Drawing.Point(132, 151)
+        Me.cmdCheck.Name = "cmdCheck"
+        Me.cmdCheck.Size = New System.Drawing.Size(75, 26)
+        Me.cmdCheck.TabIndex = 8
+        Me.cmdCheck.Text = "Check"
+        Me.cmdCheck.UseVisualStyleBackColor = True
+        '
+        'txtStoreDetails
+        '
+        Me.txtStoreDetails.Location = New System.Drawing.Point(314, 35)
+        Me.txtStoreDetails.Name = "txtStoreDetails"
+        Me.txtStoreDetails.Size = New System.Drawing.Size(246, 194)
+        Me.txtStoreDetails.TabIndex = 9
+        Me.txtStoreDetails.Text = ""
+        '
+        'cmdSave
+        '
+        Me.cmdSave.Location = New System.Drawing.Point(132, 183)
+        Me.cmdSave.Name = "cmdSave"
+        Me.cmdSave.Size = New System.Drawing.Size(75, 23)
+        Me.cmdSave.TabIndex = 10
+        Me.cmdSave.Text = "Save"
+        Me.cmdSave.UseVisualStyleBackColor = True
+        '
+        'cmdClear
+        '
+        Me.cmdClear.Location = New System.Drawing.Point(213, 165)
+        Me.cmdClear.Name = "cmdClear"
+        Me.cmdClear.Size = New System.Drawing.Size(75, 23)
+        Me.cmdClear.TabIndex = 11
+        Me.cmdClear.Text = "Clear"
+        Me.cmdClear.UseVisualStyleBackColor = True
+        '
+        'cmdLoad
+        '
+        Me.cmdLoad.Location = New System.Drawing.Point(132, 236)
+        Me.cmdLoad.Name = "cmdLoad"
+        Me.cmdLoad.Size = New System.Drawing.Size(75, 23)
+        Me.cmdLoad.TabIndex = 12
+        Me.cmdLoad.Text = "Load"
+        Me.cmdLoad.UseVisualStyleBackColor = True
+        '
+        'cmdDelete
+        '
+        Me.cmdDelete.Location = New System.Drawing.Point(132, 265)
+        Me.cmdDelete.Name = "cmdDelete"
+        Me.cmdDelete.Size = New System.Drawing.Size(75, 23)
+        Me.cmdDelete.TabIndex = 13
+        Me.cmdDelete.Text = "Delete"
+        Me.cmdDelete.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(582, 296)
+        Me.Controls.Add(Me.cmdDelete)
+        Me.Controls.Add(Me.cmdLoad)
+        Me.Controls.Add(Me.cmdClear)
+        Me.Controls.Add(Me.cmdSave)
+        Me.Controls.Add(Me.txtStoreDetails)
+        Me.Controls.Add(Me.cmdCheck)
+        Me.Controls.Add(Me.dtpDOB)
+        Me.Controls.Add(Me.txtEmail)
+        Me.Controls.Add(Me.txtLastName)
+        Me.Controls.Add(Me.txtFirstName)
+        Me.Controls.Add(Me.lblEmail)
+        Me.Controls.Add(Me.lblDOB)
+        Me.Controls.Add(Me.lblLastName)
+        Me.Controls.Add(Me.lblFirstName)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblFirstName As System.Windows.Forms.Label
+    Friend WithEvents lblLastName As System.Windows.Forms.Label
+    Friend WithEvents lblDOB As System.Windows.Forms.Label
+    Friend WithEvents lblEmail As System.Windows.Forms.Label
+    Friend WithEvents txtFirstName As System.Windows.Forms.TextBox
+    Friend WithEvents txtLastName As System.Windows.Forms.TextBox
+    Friend WithEvents txtEmail As System.Windows.Forms.TextBox
+    Friend WithEvents dtpDOB As System.Windows.Forms.DateTimePicker
+    Friend WithEvents cmdCheck As System.Windows.Forms.Button
+    Friend WithEvents txtStoreDetails As System.Windows.Forms.RichTextBox
+    Friend WithEvents cmdSave As System.Windows.Forms.Button
+    Friend WithEvents cmdClear As System.Windows.Forms.Button
+    Friend WithEvents cmdLoad As System.Windows.Forms.Button
+    Friend WithEvents cmdDelete As System.Windows.Forms.Button
+End Class
---
+++ Holidays/Holidays/Happy Hols.vb
@@ -0,0 +1,94 @@
+﻿Imports System.IO
+'Imports System
+Imports System.Text
+Public Class Form1
+    Dim ClientId As Integer = 0 'Declared globally because declaring it locally will cause the value to reset 
+
+    Private Sub cmdCheck_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdCheck.Click
+        Dim Precheck As Boolean = False 'Will be used to check if the textboxes pass the validation test
+
+        If txtEmail.Text = "" Then
+            MessageBox.Show("A textbox has been left empty.", "WARNING", MessageBoxButtons.OK)
+            Precheck = False
+        ElseIf txtFirstName.Text = "" Then
+            MessageBox.Show("A textbox has been left empty.", "WARNING", MessageBoxButtons.OK)
+            Precheck = False
+        ElseIf txtLastName.Text = "" Then
+            MessageBox.Show("A textbox has been left empty.", "WARNING", MessageBoxButtons.OK) 'Will warn the user if a textbox has been left empty and will prevent the data from getting loaded
+            Precheck = False
+        Else : Precheck = True 'Will allow the data to be appended into txtStoreDetails
+        End If
+
+
+        If Precheck = True Then
+            ClientId = ClientId + 1 'Makes the client id increase with each customer
+            txtStoreDetails.AppendText(vbTab & "  Client ID:  " & ClientId & vbNewLine) 'will load all the textboxes data into txtStoreDetails which allows me to format the data through this textbox before it gets saved
+            txtStoreDetails.AppendText("" + vbNewLine)
+            txtStoreDetails.AppendText(vbTab & lblFirstName.Text + "  :  " + txtFirstName.Text + vbNewLine) 'vbtab and vbnewline is used to make it formatted better when it is saved to the text file
+            txtStoreDetails.AppendText("" + vbNewLine) 'Creates a empty line between the data
+            txtStoreDetails.AppendText(vbTab & lblLastName.Text + "  :  " + txtLastName.Text + vbNewLine)
+            txtStoreDetails.AppendText("" + vbNewLine)
+            txtStoreDetails.AppendText(vbTab & lblDOB.Text + "  :  " + dtpDOB.Value + vbNewLine)
+            txtStoreDetails.AppendText("" + vbNewLine)
+            txtStoreDetails.AppendText(vbTab & lblEmail.Text + "  :  " + txtEmail.Text + vbNewLine)
+            txtStoreDetails.AppendText("" + vbNewLine)
+        End If
+    End Sub
+    Private Sub txtFirstName_KeyPress(ByVal sender As Object, ByVal e As KeyEventArgs) Handles txtFirstName.KeyDown, txtLastName.KeyDown 'Adding both of the textboxes to Handles... will not require a seperate sub for each textboxes
+
+        If (e.KeyCode >= Keys.D0 And e.KeyCode <= Keys.D9) Then
+        ElseIf (e.KeyCode >= Keys.NumPad0 And e.KeyCode <= Keys.NumPad9) Then 'This will prevent all number keys including the numpad from being entered into the name textboxes
+            ' This will prevent users from typing in numbers and will automatically send a backspace to remove any numbers
+            SendKeys.Send("{BackSpace}")
+        End If
+
+    End Sub
+
+    Private Sub cmdClear_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdClear.Click
+        Dim Check As Integer = MessageBox.Show("Are you sure you want to clear all your inputted data? This action will not erase saved data.", "WARNING", MessageBoxButtons.YesNo)
+
+        If Check = DialogResult.Yes Then
+            txtStoreDetails.Text = "" ' This will give the user a message to confirm if they want it to be deleted or not and if they select yes, the data stored in txtStoreDetails will be cleared
+        End If
+    End Sub
+    Private Sub cmdSave_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdSave.Click
+        Dim Path As String = "c:\vb\customerdetails.txt" 'Name of the text file
+        Dim Direct As String = "c:\vb\" 'path that will be used to store the txt file
+
+
+
+        If File.Exists(Path) = True Then
+            Dim Details As String = txtStoreDetails.Text
+            File.AppendAllText(Path, Details) ' Will add data stored in the txtStoreDetails to the txt file
+
+        Else
+
+            If Not Directory.Exists(Direct) Then
+                Directory.CreateDirectory(Direct) ' This will create the path in which the txt file will be stored in
+                Dim TextFile As System.IO.StreamWriter = My.Computer.FileSystem.OpenTextFileWriter("c:\vb\customerdetails.txt", True) 'will create the textfile as the previous line can only create the directory
+                TextFile.Close() 'Must close this afterwards otherwise the program will crash when trying to save details
+                MessageBox.Show("Directory created. Please reclick save to save your data", "", MessageBoxButtons.OK)
+            End If
+        End If
+    End Sub
+    Private Sub cmdLoad_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdLoad.Click
+
+        txtStoreDetails.Text = System.IO.File.ReadAllText("c:\vb\customerdetails.txt") 'Will load the txt file to the textbox
+
+
+        If txtStoreDetails.Text = "" Then
+            MessageBox.Show("No Data Was Found", "Error", MessageBoxButtons.OK) 'Will display an error message saying that there is no data in the file
+        End If
+
+    End Sub
+    Private Sub cmdDelete_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdDelete.Click
+        Dim Path As String = "c:\vb\customerdetails.txt" 'The path that is used to find the txt file
+        Dim Check As Integer = MessageBox.Show("Do you want to erase all stored records?", "WARNING", MessageBoxButtons.YesNo) 'Needs to be declared to use DialogResult
+
+        If Check = DialogResult.Yes Then 'Will give the user a chance to cancel the operation if they accidently click the button
+            System.IO.File.WriteAllText(Path, "") 'This will erase everything in the txt file be overwriting it with a a null character
+        End If
+
+
+    End Sub
+End Class
